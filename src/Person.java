2821499eaf2882fbcb7aebf22e837ddc31e25ceb
Amber Gardner <ambergardner@Emilys-MacBook-Air.local>
2017-04-10 15:26:46 -0500

    corrections

--- src/Person.java
+++ src/Person.java
@@ -18,11 +18,9 @@ public class Person implements Comparable{
         this.email = email;
         this.country = country;
         this.ip_address = ip_address;
-        this.details = first_name + "" + last_name + "" + country;
+        this.details = first_name + " " + last_name + " " + country;
     }
 
-    public Person(Integer id, String last_name) {
-    }
 
     public Person(String column) {
 
@@ -36,6 +34,62 @@ public class Person implements Comparable{
     public int compareTo(Object o) {
         Person otherPerson = (Person) o;
         return last_name.compareTo(otherPerson.last_name);
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+    public String getIp_address() {
+        return ip_address;
+    }
+
+    public void setIp_address(String ip_address) {
+        this.ip_address = ip_address;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+
+    public void setDetails(String details) {
+        this.details = details;
     }
 }
 
